d1efb4a7821c6daa26d55c18a8df72777a78112b
saeunnnnnni <kate6011@naver.com>
2023-04-30 22:53:25 +0900

    폴더추가

---
+++ 07.26/src/component/Sample1/AvgValue.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AvgValue = ({avg}) => {
+    return (
+        <div>
+            <h2>학생들의 영어 점수의 평균은 { avg} 입니다 </h2>
+        </div>
+    );
+};
+
+export default AvgValue;
